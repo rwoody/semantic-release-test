22c46086522a09751e8f0f58e204b3e62b987563
rwoody <devrwoody@gmail.com>
2018-12-11 12:30:28 -0600

    feat(math): Add multiplier method
    The multiplier() method creates a function that will multiply a constant value by input numbers
    
    BREAKING CHANGE: These are the breaking changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 const adder = module.exports.adder = x => y => x + y;
 
+const multiplier = module.exports.multiplier = x => y => x * y;
+
--- tests.js
+++ tests.js
@@ -1,8 +1,12 @@
 const assert = require('assert')
-const {adder} = require('./src')
+const {adder, multiplier} = require('./src')
 
 const add3 = adder(3)
-
 assert.strictEqual(typeof add3, 'function')
 assert.strictEqual(add3(6), 9)
 assert.strictEqual(add3(15), 18)
+
+const mult4 = multiplier(4)
+assert.strictEqual(typeof mult4, 'function')
+assert.strictEqual(mult4(6), 24)
+assert.strictEqual(mult4(15), 60)
